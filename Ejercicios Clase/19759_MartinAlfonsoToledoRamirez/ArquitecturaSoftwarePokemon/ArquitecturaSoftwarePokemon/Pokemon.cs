fb7c5ba8ae3de9a995f95369b0505e2aa8362454
Martin Toledo <vizardespa@gmail.com>
2013-09-13 20:49:33 -0700

    trabajo clase
    sjdhvkwjdwebvkwjn

---
+++ Ejercicios Clase/19759_MartinAlfonsoToledoRamirez/ArquitecturaSoftwarePokemon/ArquitecturaSoftwarePokemon/Pokemon.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquitecturaSoftwarePokemon
+{
+    public abstract class Pokemon
+    {
+
+
+        public Pokemon()
+        {
+            ContadorDePokemones.contador++;
+        }
+
+        //state
+        //public int vida { get; set; }
+        //public  int nivel { get; set; }
+        public string Tipo { get; set; }
+        public string Genero { get; set; }
+        public abstract int NumeroPokedex {get;}
+        public float Estatura { get; set; }
+        public float Peso { get; set; }
+
+        //public string Nombre { get; set; }
+        private string _Nombre;
+        public string Nombre
+        {
+            get
+            {
+                return _Nombre;
+            }
+            set
+            {
+                _Nombre = value;
+            }
+        }
+
+
+        //behavior
+        public abstract void Ataca();
+        public virtual void Saluda()
+        {
+            Console.WriteLine(Nombre + ": ");
+        }
+
+
+    }
+}
---
+++ Ejercicios Clase/19759_MartinAlfonsoToledoRamirez/ArquitecturaSoftwarePokemon/ArquitecturaSoftwarePokemon/Snorlax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquitecturaSoftwarePokemon
+{
+    class Snorlax : Pokemon
+    {
+        public Snorlax()
+        {
+            Nombre = "Pikachu";
+        }
+
+        public override int NumeroPokedex
+        {
+            get { return 143; }
+        }
+
+        public override void Ataca()
+        {
+            Console.WriteLine(Nombre + ": Headbutt!!! --> Snooooor!!!");
+        }
+
+        public override void Saluda()
+        {
+            //base.Saluda();
+            Console.WriteLine(Nombre + ": Snorlax Snorlax :D");
+        }
+
+        public void Dormir()
+        {
+            //base.Saluda();
+            Console.WriteLine(Nombre + ": ZZZzzzZZZzzzZZZ");
+        }
+    }
+}
